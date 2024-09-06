7d7a2530a455ca897308b4470c5c83b4f358a591
Nate Lebherz <nlebherz44@gmail.com>
2024-09-05 23:55:38 -0700

    add URL functionality for placing a bid on a product

--- ICS_auction/settings.py
+++ ICS_auction/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Default primary key field type
--- auctions/urls.py
+++ auctions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import product_list
+from .views import product_list, signup, place_bid
 
 
 urlpatterns = [
     path('', product_list, name='product_list'),  # Maps the root URL of the app to the product_list view
+    path('place-bid/<int:product_id>/', place_bid, name='place_bid'),  # URL for placing a bid
 
     # If wanting to place a bid
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/signup/', signup, name='signup'),
 ]
