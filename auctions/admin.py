d1ed6bcbd2aa91dccd9e65ef18f515e49f733510
Nate Lebherz <nlebherz44@gmail.com>
2024-09-21 11:26:55 -0700

    add picture_link to available fields in ProductAdmin fields

--- auctions/admin.py
+++ auctions/admin.py
@@ -3,13 +3,13 @@ from .models import Product, Bid
 
 class ProductAdmin(admin.ModelAdmin):
     # Display fields in the admin list view
-    list_display = ('name', 'starting_bid', 'current_bid', 'highest_bid', 'end_date')
+    list_display = ('name', 'starting_bid', 'current_bid', 'highest_bid', 'end_date', 'picture_link')
 
     # Make highest_bid a read-only field in the edit view
     readonly_fields = ('highest_bid',)
 
     # Customize form view (optional)
-    fields = ('name', 'description', 'starting_bid', 'current_bid', 'highest_bid', 'end_date')
+    fields = ('name', 'description', 'starting_bid', 'current_bid', 'highest_bid', 'end_date', 'picture_link')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Bid)
